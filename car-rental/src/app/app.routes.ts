be3c70030b947bbba1d0676f3dcd8b4dc43d3201
kasia.kordys <kasia.kordys@gmail.com>
2026-01-31 21:42:49 +0200

    add component structure, routing and home page ui

--- car-rental/src/app/app.routes.ts
+++ car-rental/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { BookingComponent } from './pages/booking/booking.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MyBookingComponent } from './pages/my-booking/my-booking.component';
+import { SearchComponent } from './pages/search/search.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+  { path: 'home', component: HomeComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'booking', component: BookingComponent },
+  { path: 'my-booking', component: MyBookingComponent },
+
+  // optional: catch-all route
+  { path: '**', redirectTo: 'home' },
+];
