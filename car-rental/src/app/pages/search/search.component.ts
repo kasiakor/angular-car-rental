ac269bd29fc89111d2a2a1c347886dbb713240ce
kasia.kordys <kasia.kordys@gmail.com>
2026-02-10 22:37:08 +0200

    add search logic to display cars by locationId

--- car-rental/src/app/pages/search/search.component.ts
+++ car-rental/src/app/pages/search/search.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { ICar } from '../../interfaces/car.interface';
+import { LocationService } from '../../services/location.service';
 
 @Component({
   selector: 'app-search',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './search.component.html',
-  styleUrl: './search.component.css'
+  styleUrl: './search.component.css',
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  constructor(private activatedRoute: ActivatedRoute) {}
+  locationId: number = 0;
+  carsByLocation: ICar[] = [];
+  isLoading = true;
 
+  locationService = inject(LocationService);
+
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.subscribe((params) => {
+      const id = params.get('locationId');
+
+      if (!id) return;
+
+      this.locationId = Number(id);
+      this.loadCars();
+    });
+  }
+
+  loadCars() {
+    this.isLoading = true;
+
+    this.locationService.getAllCarsByLocationId(this.locationId).subscribe({
+      next: (res) => {
+        this.carsByLocation = res.data;
+        this.isLoading = false;
+        console.log('this.carsByLocation', res.data);
+      },
+      error: (err) => {
+        console.error(err);
+        this.isLoading = false;
+      },
+    });
+  }
 }
